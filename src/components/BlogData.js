13899a18f9c9b263f1321cef23ff7a5edfa7d7d3
chrishester <cjhester23@gmail.com>
2022-09-15 15:21:33 -0700

    first commit

---
+++ src/components/BlogData.js
@@ -0,0 +1,48 @@
+import astronaut from "../assets/Images/test5.png";
+import astronaut1 from "../assets/Images/IMG_0086.jpeg";
+import astronaut2 from "../assets/Images/STP_Logo5.PNG";
+import astronaut3 from "../assets/Images/IMG_8397.jpeg";
+import astronaut4 from "../assets/Images/shortnew.png";
+
+export const Blogs = [
+  {
+    id: 1,
+    name: "Inclusive brand offering timeless one of a kind garments",
+    tags: ["@cami.apparel"],
+    date: "Fashion Design",
+    imgSrc: astronaut1,
+    link: "https://camiapparel.com/",
+  },
+  {
+    id: 2,
+    name: "Handmade Moisturizing Bath & Body Products",
+    tags: ["@lekbodyessentials"],
+    date: "Beauty, cosmetic & personal care",
+    imgSrc: astronaut3,
+    link: "https://www.instagram.com/lekbodyessentials/",
+  },
+  {
+    id: 3,
+    name: "Simplifying the game ",
+    tags: ["@stptraining_"],
+    date: "Basketball Skill Training",
+    imgSrc: astronaut2,
+    link: "https://www.instagram.com/stptraining_/",
+  },
+  {
+    id: 4,
+    name: "To enhance your beauty never to cover it",
+    tags: ["@CamilleRoseMagic"],
+    date: "Professional Makeup Artist",
+    imgSrc: astronaut,
+    link: "https://www.instagram.com/camillerosemagic/",
+  },
+  {
+    id: 5,
+    name: "Empowering people to create their extraordinary lives",
+    tags: ["@cocoplanmylife"],
+    date: "Life Coaching & Consulting",
+    imgSrc: astronaut4,
+    link: "https://www.instagram.com/cocoplanmylife/",
+  },
+];
---
+++ src/components/WorkData.js
@@ -0,0 +1,62 @@
+export const Work = [
+  {
+    id: 1,
+    name: "Zada Luxury",
+    description:
+      "Users are able to configure a personal profile, shop, and make secure payments via Stripe within an iOS mobile application.",
+    tags: ["iOS", "Swift", "Xcode"],
+
+    demo: "https://github.com/cjhester1/Zada",
+    github: "https://github.com/cjhester1/Zada",
+  },
+  {
+    id: 2,
+    name: "Overall Security Inc.",
+    description:
+      "Fully Responsive React.js site created for a leading security company in the San Francisco Bay Area.",
+    tags: ["React", "Smooth Scroll"],
+
+    demo: "https://overallsecurity.netlify.app/",
+    github: "https://overallsecurity.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "Object Oriented Programming",
+    description:
+      "Object Orieneted Projects implemented using C++ . Includes tic-tac-toe, Microsoft Paint simulator and more.",
+    tags: ["C++", "openGL", "OOP"],
+
+    demo: "https://github.com/cjhester1/CSE165",
+    github: "https://github.com/cjhester1/CSE165",
+  },
+  {
+    id: 4,
+    name: "React.js",
+    description:
+      "Gallery project using optimal scroll based animations for images. Optimizing UX using Framer motion and locomotive-scroll.",
+    tags: ["React.js", "VS Code", "UX"],
+
+    demo: "https://github.com/cjhester1/React.js-Gallery",
+    github: "https://github.com/cjhester1/React.js-Gallery",
+  },
+  {
+    id: 5,
+    name: "Algorithm Development",
+    description:
+      "Implementation of numerous algorithms including Linked Lists, Binary Trees, Hash Tables and more.",
+    tags: ["Data Structures", "C++"],
+
+    demo: "https://github.com/cjhester1/CSE100/tree/master/CSE100_labs",
+    github: "https://github.com/cjhester1/CSE100/tree/master/CSE100_labs",
+  },
+  {
+    id: 6,
+    name: "Java",
+    description:
+      "Multiple functions implemented to help solidify knowledge of key programming concepts and Java syntax ",
+    tags: ["Java", "OOP", "IntelliJ"],
+
+    demo: "https://github.com/cjhester1/Rev-ph1",
+    github: "https://github.com/cjhester1/Rev-ph1",
+  },
+];
